e18c2fab61aacaaa632b880725b16bd72cb1c8bd
ADRIANO OLIVEIRA <adrianopxx@gmail.com>
2022-03-18 15:25:48 -0300

    Add files via upload
    demonstracao de acesso a atributos e metodos

---
+++ src/FundamentosComentados.java
@@ -0,0 +1,12 @@
+public class FundamentosComentados {
+    public static void main(String [] args){
+        System.out.println("Boa noite !");
+   Human joao =new Human();
+        joao.nome="joao";
+        joao.fala();
+
+        Human pedro =new Human();
+        pedro.nome="PEDROOO";
+        pedro.fala();
+    }
+}
