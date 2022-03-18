f68fed94fde201162cb21c5cb09d0709ba43b684
ADRIANO OLIVEIRA <adrianopxx@gmail.com>
2022-03-18 15:48:20 -0300

    Add files via upload
    demonstarção de metodo e o this

--- src/FundamentosComentados.java
+++ src/FundamentosComentados.java
@@ -8,5 +8,17 @@ public class FundamentosComentados {
         Human pedro =new Human();
         pedro.nome="PEDROOO";
         pedro.fala();
+
+        //instancia conta bradesco
+
+        conta bradesco =new conta();
+        bradesco.saldo=5000;  // atribui 5000 ao saldo;
+        System.out.println(bradesco.saldo);
+
+        bradesco.deposita(600); // usa o método deposita depositando 600
+
+        System.out.println(bradesco.saldo); // bate o saldo
+
+
     }
 }
