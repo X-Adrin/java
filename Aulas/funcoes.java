ef68b274287a323304e04f12169b4cd632ac762c
ADRIANO OLIVEIRA <adrianopxx@gmail.com>
2022-03-22 22:30:03 -0300

    Add files via upload
    criando um novo print

--- Aulas/funcoes.java
+++ Aulas/funcoes.java
@@ -4,9 +4,14 @@ public class funcoes {
         Funcao (11,4);
         Funcao (22,10);
         Funcao (10,1);
+        printt("ola pessoal eu sou o novo print");
 
     }
     static void Funcao(int x, int y){
         System.out.println(x+y);
+    }
+
+    static void printt (String t){
+        System.out.println(t);
     }
 }
