3ec378b99f667087c7925a61116ee0aafd668b08
ADRIANO OLIVEIRA <adrianopxx@gmail.com>
2022-03-22 21:53:04 -0300

    Add files via upload
    pasta com aulas onde devo definir  o que cada arquivo faz

---
+++ Aulas/funcoes.java
@@ -0,0 +1,12 @@
+public class funcoes {
+    public static void main (String [] args){
+        Funcao (12,14);
+        Funcao (11,4);
+        Funcao (22,10);
+        Funcao (10,1);
+
+    }
+    static void Funcao(int x, int y){
+        System.out.println(x+y);
+    }
+}
