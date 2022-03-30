1fa26c9ee5198d5cc0a6116cccebed19798d1cb8
ADRIANO OLIVEIRA <adrianopxx@gmail.com>
2022-03-30 06:06:18 -0300

    linhas comentadas

--- Aulas/Usandoothis.java
+++ Aulas/Usandoothis.java
@@ -1,6 +1,7 @@
 public class Usandoothis {
            private String nome;
 
+           //eu uso o this para que a variavél não seja confundida coma do método
         public void adicionarNome(String nome) {
             this.nome = nome;
         }
